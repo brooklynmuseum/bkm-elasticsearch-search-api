99b7538518501317eda9ea4bccff70ffb908b4f3
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-12-16 10:56:55 -0500

    list layout

--- src/components/search/searchResult.tsx
+++ src/components/search/searchResult.tsx
@@ -13,15 +13,13 @@ import type { ElasticsearchDocument } from '@/types';
 
 export function SearchResult({ result }: { result: ElasticsearchDocument }) {
   return (
-    <Card>
+    <Card className="cursor-pointer transition-all hover:bg-accent hover:text-accent-foreground">
       <CardHeader className="p-4">
         <CardTitle className="text-base">{result.title}</CardTitle>
         <CardDescription className="uppercase font-semibold">{result.type}</CardDescription>
       </CardHeader>
       {result.description && (
-        <CardContent>
-          <p>{result.description}</p>
-        </CardContent>
+        <CardContent className="px-4 pb-4 text-sm">{result.description}</CardContent>
       )}
       {result.url && (
         <CardFooter>
