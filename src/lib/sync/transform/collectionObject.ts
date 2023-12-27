25f6ef76ddded2af575adadfbc4312fb2f93f2de
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-12-27 14:50:49 -0500

    fix bad artwork end year

--- src/lib/sync/transform/collectionObject.ts
+++ src/lib/sync/transform/collectionObject.ts
@@ -27,6 +27,13 @@ const transform: ElasticsearchTransformFunction = (
   // possible range beyond unix epoch, e.g. -500 (500 BCE)
   setIfHasValue(esDoc, 'startYear', sanityDoc.objectDateBegin);
   setIfHasValue(esDoc, 'endYear', sanityDoc.objectDateEnd);
+  if (esDoc.startYear && esDoc.startYear > 0 && esDoc.endYear === 0) {
+    // TODO: Bug in TMS/Sanity data: objectDateEnd is sometimes 0
+    // even though objectDateBegin is set.  In this case, set endYear
+    // to startYear.
+    esDoc.endYear = esDoc.startYear;
+  }
+
   setIfHasValue(esDoc, 'collection', getCollectionName(sanityDoc.collectionId));
   setIfHasValue(esDoc, 'museumLocation', getMuseumLocationDescription(sanityDoc.museumLocationId));
   esDoc.visible = getBooleanValue(sanityDoc.visible);
